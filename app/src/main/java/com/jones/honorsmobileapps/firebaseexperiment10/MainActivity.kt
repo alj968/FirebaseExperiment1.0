70d5c0bade73b6b171f4360fedc420cfda2cbd46
amandajones <alj968@gmail.com>
2023-04-26 10:31:03 -0400

    Can add to db, loop through all entries, and remove whole table

--- app/src/main/java/com/jones/honorsmobileapps/firebaseexperiment10/MainActivity.kt
+++ app/src/main/java/com/jones/honorsmobileapps/firebaseexperiment10/MainActivity.kt
@@ -1,62 +1,64 @@
 package com.jones.honorsmobileapps.firebaseexperiment10
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.jones.honorsmobileapps.firebaseexperiment10.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var dbRef : DatabaseReference
+    lateinit var dbRef: DatabaseReference
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         dbRef = Firebase.database.reference
 
-        addTeachersToDatabase()
+        // HOW TO: ADD TO DATABASE
+        binding.addTeacherButton.setOnClickListener {
+            val name = binding.name.text.toString()
+            val subject = binding.subject.text.toString()
+            val yearsString = binding.yearsIn.text.toString()
+            val years = Integer.parseInt(yearsString)
+            val teacher = Teacher(name, subject, years)
+            // Using .push() puts it under a unique id every time
+            dbRef.child("teachers").push().setValue(teacher)
+        }
 
-        updateFromDatabase()
+        // HOW TO: DELETE ALL ENTRIES WITHIN A TABLE IN DATABASE
+        binding.deleteTeacherButton.setOnClickListener {
+            dbRef.child("teachers").removeValue()
+        }
 
-//        dbRef.child("teachers").removeValue()
-
-//
-//        // Child is a "node"
-//        database.reference.child("message2").setValue("Goodbye, World")
-//        database.reference.child("message2").removeValue()
-//
-//        database.reference.child("message").setValue("Hello, NEW World")
-
-        //val myRef = database.getReference("message")
-
-    }
-
-    fun addTeachersToDatabase() {
-        val teacher1 = Teacher("Amanda Jones", "Comp Sci", 8);
-        val teacher2 = Teacher("Chris Sedon", "Comp Sci", 2);
-        val teacher3 = Teacher("Vince Pricci", "Math", 20);
-
-        // Push puts each under a unique id within teachers key
-        dbRef.child("teachers").push().setValue(teacher1)
-        dbRef.child("teachers").push().setValue(teacher2)
-        dbRef.child("teachers").push().setValue(teacher3)
-    }
-
-    fun updateFromDatabase() {
+        // HOW TO: UPDATE YOUR APP WILL VALUES FROM DATABASE
+        // This listener gets called when app first opens + when database is updated on the web-side
         dbRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for( childSnapshot in dataSnapshot.children) {
-                    for (child in childSnapshot.children) {
-                        Log.d("MainActivity", "Name is: ${child.child("name").getValue()}")
-                        Log.d("MainActivity", "Subject is: ${child.child("subject").getValue()}")
-                        Log.d("MainActivity", "Years in is: ${child.child("yearsIn").getValue()}")
+                // HOW TO : ACCESS OBJECT WITH ALL ENTRIES WITHIN THE DATABASE
+                val allDBEntries = dataSnapshot.children
+
+
+                var numOfTeachersAdded = 0
+                // HOW TO : ACCESS EACH VALUE IN DB (in this case, just logging to make sure it matches db)
+                for (allTeacherEntries in allDBEntries) {
+                    for (singleTeacherEntry in allTeacherEntries.children) {
+                        numOfTeachersAdded++
+                        val name = singleTeacherEntry.child("name").getValue()
+                        Log.d("MainActivity", "Name is: $name")
+                        val subject = singleTeacherEntry.child("subject").getValue()
+                        Log.d("MainActivity", "Subject is: $subject")
+                        val years = singleTeacherEntry.child("yearsIn").getValue()
+                        Log.d("MainActivity", "Years in is: $years")
                     }
                 }
+                binding.numOfTeachers.text = "$numOfTeachersAdded"
             }
-
 
             override fun onCancelled(error: DatabaseError) {
                 // Failed to read value
@@ -64,4 +66,5 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
 }
